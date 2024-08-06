899737b122492493ed0528643317777b7a3a5e36
lexon <lexonnewb@foxmail.com>
2024-08-06 21:03:10 +0800

    change desktop

--- os/configuration.nix
+++ os/configuration.nix
@@ -21,8 +21,8 @@
   services.xserver.enable = true;
 
   # Enable the XFCE Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
